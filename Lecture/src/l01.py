0bfec0844670c33631fac5d4bf18c59b85563388
Jaspreet Jawanda <147340154+sayanwala117@users.noreply.github.com>
2025-01-08 19:45:17 -0500

    Add files via upload

---
+++ Lecture/src/l01.py
@@ -0,0 +1,48 @@
+"""
+-------------------------------------------------------
+Lecture 2 Lesson 1 
+-------------------------------------------------------
+Author:  Jaspreet Jawanda
+ID:      169083252
+Email:   jawa3252@mylaurier.ca
+__updated__ = "2025-01-08"
+-------------------------------------------------------
+"""
+
+# Public and Private
+"""
+Notes:
+Public and Private Attributes:
+    - Public and Private methods are the 
+    differeance between being the user and others
+    - Python and java have different public and private 
+    - Differenced based on how its name
+    - No way to enforce private or public
+        - Public: Methods / Attributes for user 
+        - Private: Methods / Attributes (Designer)
+"""
+# Classes
+"""
+Defining Classes
+Two parts: 
+    - The name of a class
+    - The class constructor. the Constructor is always named
+    __init__ (underscores play an important role in Python 
+    variable in method naming, as we will see.
+
+Every class will have a NAME and a defining method which is init
+
+class NAME:
+    def __init__(self , value):
+        self.VALUE = VALUE
+"""
+# Objects
+"""
+Creating an object
+To create an object we need to call an constuctor 
+     object = NAME([VALUE])
+Never call the dunderinit as python itself
+"""
+
+
+
